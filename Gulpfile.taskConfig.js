62ea9017bec1eacd428540e4330ee7ca1b011102
Evan <csh11122@sina.com>
2016-06-22 15:30:02 +0800

    update

--- Gulpfile.taskConfig.js
+++ Gulpfile.taskConfig.js
@@ -1,12 +1,12 @@
-var gulp = require("gulp"),
-    browserify = require("browserify"),
-    babelify = require("babelify"),
-    source = require("vinyl-source-stream");
+var gulp = require('gulp'),
+    browserify = require('browserify'),
+    babelify = require('babelify'),
+    source = require('vinyl-source-stream');
 
 module.exports = {
     progress: function() {
         return browserify('./demos/progress/app.js')
-            .transform("babelify", { presets: ["es2015", "react"] })
+            .transform('babelify', { presets: ['es2015', 'react'] })
             .bundle()
             .pipe(source('bundle.js'))
             .pipe(gulp.dest('./demos/progress'));
