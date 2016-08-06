a532223d5559c273d857fd74909c4ab9f10291e3
Evan <csh11122@sina.com>
2016-08-06 16:21:33 +0800

    add es6

--- react-with-webpack/public/js/logo.js
+++ react-with-webpack/public/js/logo.js
@@ -1,5 +1,17 @@
-require('../css/logo.css');
-var React=require('react');
+// require('../css/logo.css');
+// var React=require('react');
+// var Logo = React.createClass({
+//     render:function(){
+//         return (
+//             <img className="logo" src={require('../images/logo.png')} />
+//         );
+//     }
+// });
+// module.exports=Logo;
+
+//------es6写法
+import '../css/logo.css';
+import React from 'react';
 var Logo = React.createClass({
     render:function(){
         return (
--- react-with-webpack/webpack.config.js
+++ react-with-webpack/webpack.config.js
@@ -1,24 +1,20 @@
-module.exports={
-    entry:'./public/js/app.js',
-    output:{
-        path:'./public/build',
-        publicPath:'./build',
-        filename:'bundle.js'
+module.exports = {
+    entry: './public/js/app.js',
+    output: {
+        path: './public/build',
+        publicPath: './build',
+        filename: 'bundle.js'
     },
-    module:{
-        loaders:[
-            {
-                test:/\.(js)$/,
-                loader:'jsx-loader?harmony'
-            },
-            {
-                test:/\.(css)$/,
-                loader:'style-loader!css-loader'
-            },
-            {
-                test:/\.(png|jpg)$/,
-                loader:'url-loader?size=8192'
-            }
-        ]
+    module: {
+        loaders: [{
+            test: /\.(js)$/,
+            loader: 'babel-loader'
+        }, {
+            test: /\.(css)$/,
+            loader: 'style-loader!css-loader'
+        }, {
+            test: /\.(png|jpg)$/,
+            loader: 'url-loader?size=8192'
+        }]
     }
 }
